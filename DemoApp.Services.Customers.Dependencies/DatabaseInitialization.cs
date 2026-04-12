32d8db9957870c1cbc220ef7cca47e6877dc87d2
Matt Astill <matt@surianming.co.uk>
2026-04-12 15:23:17 +0100

    Adding some logging to test stuff

--- DemoApp.Services.Customers.Dependencies/DatabaseInitialization.cs
+++ DemoApp.Services.Customers.Dependencies/DatabaseInitialization.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using SmingCode.Utilities.StartupProcesses.AspNetCore;
 
 namespace DemoApp.Services.Customers.Dependencies;
@@ -7,8 +8,18 @@ using Databases.Customers.Context;
 internal class DatabaseInitialization : IServiceInitializer
 {
     public Delegate ServiceInitializer =>
-        (CustomerContext customerContext) =>
+        (
+            CustomerContext customerContext,
+            ILogger<DatabaseInitialization> logger
+        ) =>
         {
+            logger.LogInformation(
+                "Got inside database initialization. db connection string is {connString}",
+                customerContext.Database.GetConnectionString()
+            );
+
+            File.WriteAllText("/data/test-write.txt", "Hello there");
+
             customerContext.Database.Migrate();
         };
 }
